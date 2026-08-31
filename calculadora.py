2206a17f2dd5d6dffe246aa0a5ba349ea3d96eca
Luiz Alves <luizhalves.estudos@gmail.com>
2026-08-31 01:37:04 -0300

    Adicionar arquivo calculadora.py
    Primeira versão da calculadora desenvolvida em Python para praticar variáveis, entrada de dados e operações matemáticas.

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+print("Soma:", numero1 + numero2)
+print("Subtração:", numero1 - numero2)
+print("Multiplicação:", numero1 * numero2)
+print("Divisão:", numero1 / numero2)
+
+print("Obrigado por usar minha primeira calculadora! Ass. Luiz Alves")
